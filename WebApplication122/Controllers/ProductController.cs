4d4ae7e362b155fffd979d34a88289d05f2b6bd6
yesaswini567 <109148350+yesaswini567@users.noreply.github.com>
2022-08-29 15:27:15 +0530

    Add files via upload

---
+++ WebApplication122/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication122.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public List<MockProduct> GetProductList()
+        {
+            List<MockProduct> mockProducts = new List<MockProduct>();
+            MockProduct mp = new MockProduct();
+            mp.ID = 1;
+            mp.ProductName = "Product1";
+            mockProducts.Add(mp);
+            return mockProducts;
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProductList([FromBody] MockProduct mockProduct)
+        {
+            //Do Logic
+            return Ok();
+
+        }
+    }
+    public class MockProduct
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
